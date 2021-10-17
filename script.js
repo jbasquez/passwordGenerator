7e69d8938beb32a5d813df8273af0adcea9fcd41
Joe Basquez <jybasquez@yahoo.com>
2021-10-16 22:32:04 -0400

    password 100% returning correctly

--- script.js
+++ script.js
@@ -15,7 +15,10 @@ const randomFunc = {
 };
 
 generateEl.addEventListener('click', () => {
-    console.log("ive been clicked");
+    // length value grabbed from html converted from string to number w/ +
+    const length = +lengthEl.value;
+    // console.log(length);
+    // console.log("ive been clicked");
     //
     var settings;
 
@@ -35,94 +38,100 @@ generateEl.addEventListener('click', () => {
     upperLetter = alpha.map(toUpper);
 
     if(lowercaseEl.checked && uppercaseEl.checked && symbolsEl.checked&& numbersEl.checked === true) {
-        console.log("has all");
+        // console.log("has all");
         settings = specCharacter.concat(numbers, alpha, upperLetter);
-        console.log(settings[0]);
-        console.log(settings[1]);
+        // console.log(settings[0]);
+        // console.log(settings[1]);
     }else if(uppercaseEl.checked && symbolsEl.checked && numbersEl.checked === true ){
         settings = specCharacter.concat(numbers,upperLetter);
-        console.log(settings);
+        // console.log(settings);
     }else if(uppercaseEl.checked && numbersEl.checked && lowercaseEl.checked === true ){
         settings = alpha.concat(numbers, upperLetter);
-        console.log(settings);
+        // console.log(settings);
     }else if(uppercaseEl.checked && symbolsEl.checked && lowercaseEl.checked === true ){
         settings = upperLetter.concat(specCharacter, alpha);
-        console.log(settings);
+        // console.log(settings);
     }else if(symbolsEl.checked && numbersEl.checked && lowercaseEl.checked === true ){
         settings = specCharacter.concat(numbers, alpha);
-        console.log(settings);
+        // console.log(settings);
     }else if(uppercaseEl.checked && symbolsEl.checked === true ){
         settings = upperLetter.concat(specCharacter);
-        console.log(settings);
+        // console.log(settings);
     }else if(uppercaseEl.checked && numbersEl.checked=== true ){
         settings = upperLetter.concat(numbers);
-        console.log(settings);
+        // console.log(settings);
     }else if(uppercaseEl.checked && lowercaseEl.checked === true ){
         settings = upperLetter.concat(alpha);
-        console.log(settings);
+        // console.log(settings);
     }else if(lowercaseEl.checked && symbolsEl.checked === true ){
         settings = alpha.concat(specCharacter);
-        console.log(settings);
+        // console.log(settings);
     }else if(lowercaseEl.checked && numbersEl.checked=== true ){
         settings = alpha.concat(numbers);
-        console.log(settings);
+        // console.log(settings);
     }else if(numbersEl.checked && symbolsEl.checked=== true ){
         settings = numbers.concat(specCharacter);
-        console.log(settings);
+        // console.log(settings);
     }else if(lowercaseEl.checked === true ){
         settings = alpha;
-        console.log(settings);
+        // console.log(settings);
     }else if(uppercaseEl.checked === true ){
         settings = upperLetter;
-        console.log(settings);
+        // console.log(settings);
     }else if(numbersEl.checked === true ){
         settings = numbers;
-        console.log(settings);
+        // console.log(settings);
     }else if(symbolsEl.checked === true ){
         settings = specCharacter;
-        console.log(settings);
+        // console.log(settings);
     }else{
-        console.log("needs a value");
+        // console.log("needs a value");
         alert("please select a input");
         //event.preventDefault();
     }
-    for(var i = 0; i < 8; i++){
-        var pass;
-        pass += settings[i];
-        console.log(pass); 
-        console.log("helooooo");
-    };
-    console.log(pass);
+    // console.log(pass);
     //for loop to take user input loop through "i" as long as length and push the final password into a array
     // const length = lengthEl.value;
-    // const hasLower = lowercaseEl.checked;
-    // const hasUpper = uppercaseEl.checked;
-    // const hasNumber = numbersEl.checked;
-    // const hasSymbol = symbolsEl.checked;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+    // console.log(hasLower,hasUpper,hasNumber,hasSymbol, length)
     
-    // resultEl.innerText = generatePassword(
-    //     hasLower,
-    //     hasUpper,
-    //     hasNumber,
-    //     hasSymbol,
-    //     length
-    // );
+    resultEl.innerText = generatePassword(
+        hasLower,
+        hasUpper,
+        hasNumber,
+        hasSymbol,
+        length
+    );
 });
 
 function generatePassword(lower, upper, number, symbol, length) {
     
    let generatedPassword = '';
     
-    const typesCount = lower + upper + number + symbols;
+    const typesCount = lower + upper + number + symbol;
+     console.log('typesCount:', typesCount);
     
-   const typesArr = [{lower}, {upper}, {number}, {symbol}].filter
-       (
-       item => Object.values(item)[0]
-       );
+   const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0])
+   console.log(typesArr);
     
    if(typesCount === 0) {
        return '';
    }
+   for(let i = 0; i < length; i++){
+       typesArr.forEach(type =>{
+           const pushedItem = Object.keys(type)[0];
+           console.log(pushedItem);
+           generatedPassword += randomFunc[pushedItem]();
+       })
+    // var pass;
+    // pass += settings[i];
+
+    //  console.log(pass); 
+    // console.log("helooooo");
+    };
   
     const finalPassword = generatedPassword.slice(0, length);
    return finalPassword;
@@ -139,6 +148,7 @@ function getRandomNumber() {
     return String.fromCharCode(Math.floor(Math.random()*10)+48);
 }
 function getRandomSymbols() {
-    const symbols = '!@#$%^&*';
+    const symbols = '!@#$%^&*{}[]></.,';
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
+console.log(getRandomSymbols());
